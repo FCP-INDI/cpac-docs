392b5bd35423434cc6d7699cb529989ae804eb8e
Jon Cluce <jon.clucas@childmind.org>
2025-10-28 13:04:45 -0400

    :wrench: Update vite configuration for bundling node dependencies

--- dev/build.js
+++ dev/build.js
@@ -1,5 +1,4 @@
 const { execSync } = require("child_process");
-
 const outDir = process.env.OUTDIR || "./cpac-docs/develop";
 console.log(`Building to ${outDir}`);
-execSync(`tsc --outDir ${outDir} --target es6`, { stdio: "inherit" });
+execSync(`vite build`, { stdio: "inherit", env: { ...process.env, OUTDIR: outDir } });
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,33 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+const htmlFiles = globSync('src/**/*.html');
+const input = Object.fromEntries(
+  htmlFiles.map(file => [
+    file.slice(4, -5), // Remove 'src/' and '.html'
+    resolve(__dirname, file)
+  ])
+);
 
 export default defineConfig({
   root: 'src',
+  base: process.env.DOCS_VERSION 
+    ? `/cpac-docs/${process.env.DOCS_VERSION}/`
+    : '/cpac-docs/develop/',
   build: {
-    outDir: '../cpac-docs',
-    emptyOutDir: true
+    outDir: process.env.OUTDIR || '../cpac-docs/develop',
+    emptyOutDir: false,
+    rollupOptions: {
+      input
+    }
   },
   server: {
     headers: {
       'Cache-Control': 'no-store'
     },
     port: 5173
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
   }
 });
